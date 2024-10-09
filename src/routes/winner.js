a01714327b813743df2d44cd7a4b0535092d404e
Leonardo Santos <leonardo.santos574@hotmail.com>
2024-10-08 22:56:18 -0300

    ajuste rota

--- src/routes/winner.js
+++ src/routes/winner.js
@@ -2,8 +2,9 @@ const winnerRouter  = require("express").Router()
 const winnerController = require("../controllers/WinnerController")
 const validateWinnerSchema = require("../middlewares/validateWinnerSchema")
 
-winnerRouter.get("/:id",winnerController.findWinnerByIdController)
+
 winnerRouter.get("/",winnerController.findAllWinnersController)
+winnerRouter.get("/:id",winnerController.findWinnerByIdController)
 winnerRouter.post("/",validateWinnerSchema, winnerController.createWinnerController)
 winnerRouter.delete("/:id",winnerController.deleteWinnerByIdController)
 
