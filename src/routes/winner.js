22b82de885331d6f52efdc72a4c558ac71aaaf9b
Leonardo Santos <leonardo.santos574@hotmail.com>
2024-10-08 23:02:24 -0300

    routes

--- src/routes/winner.js
+++ src/routes/winner.js
@@ -2,9 +2,8 @@ const winnerRouter  = require("express").Router()
 const winnerController = require("../controllers/WinnerController")
 const validateWinnerSchema = require("../middlewares/validateWinnerSchema")
 
-
-winnerRouter.get("/",winnerController.findAllWinnersController)
 winnerRouter.get("/:id",winnerController.findWinnerByIdController)
+winnerRouter.get("/",winnerController.findAllWinnersController)
 winnerRouter.post("/",validateWinnerSchema, winnerController.createWinnerController)
 winnerRouter.delete("/:id",winnerController.deleteWinnerByIdController)
 
